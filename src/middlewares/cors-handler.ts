ceccd7ed99f8723e049e5c52af6f2f3b5bf45eaf
huydang7 <dqhuy7@gmail.com>
2022-05-19 20:05:09 +0700

    Create cors-handler.ts

---
+++ src/middlewares/cors-handler.ts
@@ -0,0 +1,12 @@
+export default (req: any, res: any, next: Function) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  if (req.method === 'OPTIONS') {
+    return res.send();
+  }
+  return next();
+};
