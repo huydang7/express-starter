bd936bce40bb23187eabf7aafbbda178b78dada7
huydang7 <dqhuy7@gmail.com>
2022-11-10 17:30:04 +0700

    add production migration

--- database/config/connection.js
+++ database/config/connection.js
@@ -6,6 +6,13 @@ const config = {
     password: process.env.DB_PASSWORD,
     dialect: 'postgres',
   },
+  production: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    dialect: 'postgres',
+  },
 };
 
 module.exports = config;
