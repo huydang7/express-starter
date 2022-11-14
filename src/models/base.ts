1fe36174361185296d14e255166445d3a1c20045
huydang7 <dqhuy7@gmail.com>
2022-11-14 19:16:57 +0700

    udpate base timestamp

--- src/models/base.ts
+++ src/models/base.ts
@@ -3,9 +3,11 @@ import sequelize from 'sequelize';
 export const TimestampDefinition = {
   createdAt: {
     type: sequelize.DATE,
+    defaultValue: sequelize.fn('NOW'),
   },
   updatedAt: {
     type: sequelize.DATE,
+    defaultValue: sequelize.fn('NOW'),
   },
   deletedAt: {
     type: sequelize.DATE,
